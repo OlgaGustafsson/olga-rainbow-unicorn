668802f91c5d574ed4f54cfa0048f1c242c82d91
Olga Gustafsson <olgagustafsson@MacBook-Air-som-tillhor-Olga.local>
2023-01-16 18:23:10 +0100

    app.ts

--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
-const arr = ['apa', 'orangutang', 'gorilla', 'get', 'giraff',];
+const arr: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(arr);
 
+
+type person = {
+    name: string;
+    age: number; 
+    student: boolean;
+};
+const student: person [] = [
+    {
+        name: "Gugge",
+        age: 47, 
+        student: true,
+    };
+    
+console.log(student);
